52c60f5d461afc9a2b6e94ff058ea898bc43cff2
LLTae2 <taehyun5820@gmail.com>
2023-01-24 17:53:27 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import SnowMan from "./components/SnowMan";
+import StickChart from "./components/StickChart";
+import PieChart from "./components/PieChart";
+import LineChart from "./components/LineChart";
+import Svg from "./components/Svg";
+import E6 from "./components/E6";
+
+const Container = styled.div`
+  padding: 50px 100px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* <Svg /> */}
+      <SnowMan />
+      <StickChart />
+      <PieChart />
+      <E6 />
+    </Container>
   );
 }
 
